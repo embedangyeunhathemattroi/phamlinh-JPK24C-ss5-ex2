5c0f4e7d5b6082b251701264f4df9be88af86bf8
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2024-11-27 10:12:42 +0700

    Add files via upload

---
+++ ex2-ss5.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int so1 = 10;  
+    int so2;
+
+    printf("Nhap so ngau nhien ma dung voi so cua to: ", so1);
+
+     while (so2 != so1){
+        scanf("%d", &so2); 
+        
+        if (so2 != so1) {  
+            printf("ban lam sai roi, hay sua lai ngay nha: ");
+        }
+    } 
+
+    printf("Woaaaaaaa .Chuc mung ban ,ban that la xuat sac !\n", num1);
+
+    return 0;
+}
+
